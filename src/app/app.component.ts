c17f35b7309e7bd792644579920c533a52120d31
ricbahtias <ricky1800016085@webmail.uad.ac.id>
2020-03-30 00:38:17 +0700

    tekweb

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './halaman-utama.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tekweb1800016085';
+}
